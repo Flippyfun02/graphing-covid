edad71cae5e2aaa3c68803d53f2dd6b0674510ae
Flippyfun02 <amiebinan@gmail.com>
2021-07-28 01:59:05 -0400

    planning set up

--- covid.py
+++ covid.py
@@ -1,14 +1,19 @@
 from matplotlib import pyplot as plt
-import csv
+import cs50, csv
 
 plt.title("Covid Cases")
 plt.xlabel("Time")
 plt.ylabel("Number of Cases")
 
 # Each date has the countries' case reports
+# countries = {country_name : country}
+# country = [{date : YYYY-MM-DD, cases, etc}]
 
-with open("countries-aggregated.csv") as data:
-    reader = csv.reader(data)
-    next(reader)
+countries = []
+country = []
+with open("countries-aggregated-2021-07-26.csv") as data:
+    reader = csv.DictReader(data)
+    for i in reader:
+        
 
-plt.show()
+# plt.show()
