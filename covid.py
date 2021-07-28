e029bb94d28e5f05deebb5a28673b22ec7af5159
Flippyfun02 <amiebinan@gmail.com>
2021-07-28 01:17:40 -0400

    set up graph

--- covid.py
+++ covid.py
@@ -1,6 +1,14 @@
 from matplotlib import pyplot as plt
 import csv
 
+plt.title("Covid Cases")
+plt.xlabel("Time")
+plt.ylabel("Number of Cases")
+
+# Each date has the countries' case reports
+
 with open("countries-aggregated.csv") as data:
     reader = csv.reader(data)
     next(reader)
+
+plt.show()
