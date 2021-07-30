d617dd79d3e1e3995ebe730db4ca012d5a01eef5
Flippyfun02 <amiebinan@gmail.com>
2021-07-30 01:31:44 -0400

    fixed graph

--- covid.py
+++ covid.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+import matplotlib.dates as mdates
 from cs50 import SQL
 import datetime
 
@@ -20,14 +21,18 @@ datesDict = db.execute("SELECT Date FROM countries WHERE Country = 'Afghanistan'
 dates = []
 dates = make_list(datesDict, dates, 'Date')
 for i in range(len(dates)): # format date
-    dates[i] = datetime.datetime.strptime(dates[i], '%Y-%m-%d').strftime("%b %Y")
+    dates[i] = datetime.datetime.strptime(dates[i], '%Y-%m-%d')
+    mdates.DateFormatter('%m %Y')
 
 # create list of confirmed cases for y axis
 select_country = "United Kingdom"
 find_confirmed = db.execute("SELECT Confirmed FROM countries WHERE Country = ?", select_country)
 confirmed = []
 confirmed = make_list(find_confirmed, confirmed, "Confirmed")
+for i in range(len(confirmed)): # int case numbers
+    confirmed[i] = int(confirmed[i])
 
 # plot
 plt.plot(dates, confirmed, color = "red")
+plt.xticks(rotation = 30)
 plt.show()
