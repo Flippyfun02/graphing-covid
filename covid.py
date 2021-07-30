7c0c7c91c7d0ec387b6b157c5704e6058bfaf706
Flippyfun02 <amiebinan@gmail.com>
2021-07-30 02:03:14 -0400

    try/except, sys.argv

--- covid.py
+++ covid.py
@@ -1,12 +1,15 @@
 from matplotlib import pyplot as plt
 import matplotlib.dates as mdates
 from cs50 import SQL
-import datetime
+import datetime, sys
 
 def make_list(query, thelist, key): # format list so not dict
     for i in query:
         thelist.append(i[key])
     return thelist
+
+if len(sys.argv) != 2:
+    sys.exit("Usage: python3 covid.py 'Country'")
 
 plt.title("Covid Cases")
 plt.xlabel("Time")
@@ -22,17 +25,21 @@ dates = []
 dates = make_list(datesDict, dates, 'Date')
 for i in range(len(dates)): # format date
     dates[i] = datetime.datetime.strptime(dates[i], '%Y-%m-%d')
-    mdates.DateFormatter('%m %Y')
 
 # create list of confirmed cases for y axis
-select_country = "United Kingdom"
-find_confirmed = db.execute("SELECT Confirmed FROM countries WHERE Country = ?", select_country)
-confirmed = []
-confirmed = make_list(find_confirmed, confirmed, "Confirmed")
-for i in range(len(confirmed)): # int case numbers
-    confirmed[i] = int(confirmed[i])
+while True:
+    try: # check if country is in database
+        find_confirmed = db.execute("SELECT Confirmed FROM countries WHERE Country = ?", sys.argv[1])
+        confirmed = []
+        confirmed = make_list(find_confirmed, confirmed, "Confirmed")
+        for i in range(len(confirmed)): # int case numbers
+            confirmed[i] = int(confirmed[i])
 
-# plot
-plt.plot(dates, confirmed, color = "red")
+        # plot
+        plt.plot(dates, confirmed, color = "red")
+        break
+    except: # if not, still do not return error
+        print("Country is unavailable.")
 plt.xticks(rotation = 30)
+# ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
 plt.show()
